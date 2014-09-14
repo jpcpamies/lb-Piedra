3b02e8cd0acf92d99e361fc575453f809df796b0
Jordi Pamies <jordipamies2@gmail.com>
2014-09-14 14:39:34 +0100

    Definidas condiciones para ganar o perder

--- js/codigo.js
+++ js/codigo.js
@@ -1,6 +1,52 @@
 
 var opcionUsuario
-var opcionMaquina
+var opcionMaquina = piedra
 
 opcionUsuario = prompt ("¿Qué eliges piedra, papel o tigera?" , "piedra");
 alert("Has elegido " + opcionUsuario)
+
+if(opcionUsuario == piedra)
+{
+	if(opcionMaquina == piedra)
+	{
+		alert("Empate!!");
+	}
+	else if(opcionMaquina == papel)
+	{
+		alert("Perdiste :(");
+	}
+	else(opcionMaquina == tigera)
+	{
+		alert("Ganaste :)!!");
+	}
+}
+else if(opcionUsuario == papel)
+{
+	if(opcionMaquina == piedra)
+	{
+		alert("Ganaste :)!!");
+	}
+	else if(opcionMaquina == papel)
+	{
+		alert("Empate!!");
+	}
+	else(opcionMaquina == tigera)
+	{
+		alert("Perdiste :(!!");
+	}
+}
+else(opcionUsuario == tigera)
+{
+	if(opcionMaquina == piedra)
+	{
+		alert("Perdiste :(!!");
+	}
+	else if(opcionMaquina == papel)
+	{
+		alert("Ganaste :)!!");
+	}
+	else(opcionMaquina == tigera)
+	{
+		alert("Empate!!");
+	}
+}
