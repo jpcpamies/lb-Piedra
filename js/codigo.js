1c7015e6d96dee6aeb1beeb8741e35da774fa756
Jordi Pamies <jordipamies2@gmail.com>
2014-09-14 18:50:28 +0100

    Programado juego para 4 opciones

--- js/codigo.js
+++ js/codigo.js
@@ -16,11 +16,11 @@ var opciones = ["Piedra", "Papel", "Tijera", "Lagarto", "Spock"];
 var opcionUsuario;
 var opcionMaquina = aleatorio(0,4);
 
-opcionUsuario = prompt("¿Qué elijes: \nPiedra:0\nPapel:1\nTijera:2?\nLagarto:3\nSpock:4" , 0);
+opcionUsuario = prompt("¿Qué elijes: \nPiedra:0\nPapel:1\nTijera:2\nLagarto:3\nSpock:4?" , 0);
 alert( "Elejiste " + opciones[opcionUsuario] );
 alert( "JavaScript elijió " + opciones[opcionMaquina] );
 
-
+// Piedra
 if(opcionUsuario == 0)
 {
 	if(opcionMaquina == piedra)
@@ -35,7 +35,16 @@ if(opcionUsuario == 0)
 	{
 		alert("Ganaste");
 	}
+	else if(opcionMaquina == lagarto)
+	{
+		alert("Piedra aplasta al lagarto: Ganaste");
+	}
+	else if(opcionMaquina == spock)
+	{
+		alert("Spock vaporiza piedra: Perdiste");
+	}
 }
+// papel
 else if(opcionUsuario == 1)
 {
 	if(opcionMaquina == piedra)
@@ -50,7 +59,16 @@ else if(opcionUsuario == 1)
 	{
 		alert("Perdiste");
 	}
+	else if(opcionMaquina == lagarto)
+	{
+		alert("Lagarto come papel: Pediste");
+	}
+	else if(opcionMaquina == spock)
+	{
+		alert("Papel desaprueba Spock: Ganaste");
+	}
 }
+// tijera
 else if(opcionUsuario == 2)
 {
 	if(opcionMaquina == piedra)
@@ -62,6 +80,62 @@ else if(opcionUsuario == 2)
 		alert("Ganaste");
 	}
 	else if(opcionMaquina == tijera)
+	{
+		alert("Empate");
+	}
+	else if(opcionMaquina == lagarto)
+	{
+		alert("Tijera decapita lagarto: Ganaste");
+	}
+	else if(opcionMaquina == spock)
+	{
+		alert("Spock aplasta tijeras: Perdiste");
+	}
+}
+// lagarto
+else if(opcionUsuario == 3)
+{
+	if(opcionMaquina == piedra)
+	{
+		alert("Piedra aplasta lagarto: Perdiste");
+	}
+	else if(opcionMaquina == papel)
+	{
+		alert("Lagarto come papel: Ganaste");
+	}
+	else if(opcionMaquina == tijera)
+	{
+		alert("Tijera decapita lagarto: Perdiste");
+	}
+	else if(opcionMaquina == lagarto)
+	{
+		alert("Empate");
+	}
+	else if(opcionMaquina == spock)
+	{
+		alert("Lagarto envenena Spock: Ganaste");
+	}
+}
+// spock
+else if(opcionUsuario == 4)
+{
+	if(opcionMaquina == piedra)
+	{
+		alert("Spock vaporiza piedra: Ganaste");
+	}
+	else if(opcionMaquina == papel)
+	{
+		alert("Papel desapruba Spock: Perdiste");
+	}
+	else if(opcionMaquina == tijera)
+	{
+		alert("Spock aplasta tijera: Ganaste");
+	}
+	else if(opcionMaquina == lagarto)
+	{
+		alert("Lagarto envenena Spock: Perdiste");
+	}
+	else if(opcionMaquina == spock)
 	{
 		alert("Empate");
 	}
