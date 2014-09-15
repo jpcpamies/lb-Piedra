8cb72b94e781b40484bcc14820b94f00d9e5051d
Jordi Pamies <jordipamies2@gmail.com>
2014-09-15 18:26:05 +0100

    Reparado problema de generación de num aleatorio

--- js/codigo.js
+++ js/codigo.js
@@ -1,70 +1,70 @@
-
-// Generador de un número aleatorio
-function aleatorio(maximo, minimo)
+//Genera un número aleatorio entre un rango de enteros
+function patito(minimo, maximo)
 {
-	var numero = Math.floor(Math.random() + (maximo - minimo) + minimo);
-	return numero;
+    var numero = Math.floor( Math.random() * (maximo - minimo + 1) + minimo );
+    return numero;
 }
 
 var piedra = 0;
 var papel = 1;
 var tijera = 2;
+
 var opciones = ["Piedra", "Papel", "Tijera"];
 
 var opcionUsuario;
-var opcionMaquina = aleatorio(0,2);
+var opcionMaquina = patito(0,2);
 
-opcionUsuario = prompt("¿Qué elijes: \nPiedra:0\nPapel:1\nTijera:2?" , 0);
-alert( "Elejiste " + opciones[opcionUsuario] );
-alert( "JavaScript elijió " + opciones[opcionMaquina] );
+opcionUsuario = prompt("¿Qué eliges?\nPiedra: 0\nPapel: 1\nTijera: 2", 0);
 
+alert("Elegiste " + opciones[opcionUsuario]);
+alert("Javascript eligió " + opciones[opcionMaquina]);
 
-if(opcionUsuario == 0)
+if(opcionUsuario == piedra)
 {
-	if(opcionMaquina == piedra)
-	{
-		alert("Empate");
-	}
-	else if(opcionMaquina == papel)
-	{
-		alert("Perdiste");
-	}
-	else if(opcionMaquina == tijera)
-	{
-		alert("Ganaste");
-	}
+    if(opcionMaquina == piedra)
+    {
+        alert("Empate!");
+    }
+    else if(opcionMaquina == papel)
+    {
+        alert("Perdiste :( ");
+    }
+    else if(opcionMaquina == tijera)
+    {
+        alert("Ganaste!");
+    }
 }
-else if(opcionUsuario == 1)
+else if(opcionUsuario == papel)
 {
-	if(opcionMaquina == piedra)
-	{
-		alert("Ganaste");
-	}
-	else if(opcionMaquina == papel)
-	{
-		alert("Empate");
-	}
-	else if(opcionMaquina == tijera)
-	{
-		alert("Perdiste");
-	}
+    if(opcionMaquina == piedra)
+    {
+        alert("Ganaste!");
+    }
+    else if(opcionMaquina == papel)
+    {
+        alert("Empate!");
+    }
+    else if(opcionMaquina == tijera)
+    {
+        alert("Perdiste!");
+    }
 }
-else if(opcionUsuario == 2)
+else if(opcionUsuario == tijera)
 {
-	if(opcionMaquina == piedra)
-	{
-		alert("Perdiste");
-	}
-	else if(opcionMaquina == papel)
-	{
-		alert("Ganaste");
-	}
-	else if(opcionMaquina == tijera)
-	{
-		alert("Empate");
-	}
+    if(opcionMaquina == piedra)
+    {
+        alert("Perdiste!");
+    }
+    else if(opcionMaquina == papel)
+    {
+        alert("Ganaste!");
+    }
+    else if(opcionMaquina == tijera)
+    {
+        alert("Empate!");
+    }
 }
 else
 {
-	alert("¿Qué has hecho ahí?")
+    alert("¿Pos qué carajo?");
 }
